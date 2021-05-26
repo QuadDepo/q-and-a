3c25a2d6575996105e786083f2a54df7da52b646
QuadDepo <ryan@qdds.nl>
2021-05-26 17:38:27 +0200

    Add Header Component

---
+++ src/components/layout/Header.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { Col, Container, Row } from "./index";
+
+const Logo = styled.h1`
+  font-size: 30px;
+  color: #fff;
+
+  span {
+    &:nth-of-type(1) {
+      color: #fe5f55;
+    }
+
+    &:nth-of-type(2) {
+      color: #7ae582;
+    }
+  }
+`;
+
+const MainHeader = styled(Row)`
+  background: #000;
+`;
+
+export default function Header() {
+  return (
+    <MainHeader>
+      <Container>
+        <Col xs={12}>
+          <Logo>
+            TA<span>Q</span>&<span>A</span>T
+          </Logo>
+        </Col>
+      </Container>
+    </MainHeader>
+  );
+}
