1474118cddffadb20735b2cc13c2d4889616e42b
QuadDepo <ryan@qdds.nl>
2021-06-01 23:26:17 +0200

    Fix Container width

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,7 +2,8 @@ import { Row, Col } from "react-flexa";
 import styled from "styled-components";
 
 const Container = styled(Row)`
-  width: 1280px;
+  width: 100%;
+  max-width: 1280px;
   margin 0 auto;
   padding: 0 16px;
 `;
