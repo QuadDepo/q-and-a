5db44394ce28729e4cd7580620722a5161a819fc
QuadDepo <ryan@qdds.nl>
2021-06-08 22:37:20 +0200

    Add localstorage helpers for store

--- src/core/Helpers/index.js
+++ src/core/Helpers/index.js
@@ -1,3 +1,21 @@
-export const isObjectEmpty = (object) => {
-  return Object.values(object).every((x) => x === null || x === "");
+export const saveToLocalStorage = (state) => {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("qa_list", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+};
+
+export const initialStoreFromLocalStorage = () => {
+  try {
+    const serialisedState = localStorage.getItem("qa_list");
+    if (serialisedState === null) return undefined;
+
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+
+    return undefined;
+  }
 };
