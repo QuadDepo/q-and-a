f474fe660283b17da29056726fb3c05a9c2892f2
QuadDepo <ryan@qdds.nl>
2021-06-09 22:23:55 +0200

    Add sorting to CarouselWrapper

--- src/components/CarouselWrapper/index.js
+++ src/components/CarouselWrapper/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import Carousel from "../Carousel";
@@ -9,32 +10,46 @@ const CarouselList = styled.div`
 `;
 
 const CarouselListEmpty = styled.p`
+  box-sizing: border-box;
   width: 100%;
   background: #f39c12;
   padding: 18px;
 `;
 
 function CarouselWrapper(props) {
-  const { loading, error, qa_list, order } = props;
+  const { qa_list, order } = props;
+  const [items, setItems] = useState(qa_list);
+
+  useEffect(() => {
+    if (qa_list.length === 0) return;
+
+    const orderedItems = qa_list.sort((a, b) => {
+      if (order.direction === "ASC")
+        return a[order.key].localeCompare(b[order.key]);
+      return b[order.key].localeCompare(a[order.key]);
+    });
+
+    setItems([...orderedItems]);
+  }, [order, qa_list]);
 
   return (
     <CarouselList>
-      {!qa_list ||
-        (qa_list.length < 1 && (
+      {!items ||
+        (items.length < 1 && (
           <CarouselListEmpty>
             Oh noh! You don't have any Q/A's
           </CarouselListEmpty>
         ))}
 
-      {qa_list.map((item) => (
+      {items.map((item) => (
         <Carousel key={item.uuid} carouselItem={item} />
       ))}
     </CarouselList>
   );
 }
 
-const mapStateToProps = (state) => ({
-  ...state,
+const mapStateToProps = ({ qa_list }) => ({
+  qa_list,
 });
 
 export default connect(mapStateToProps)(CarouselWrapper);
