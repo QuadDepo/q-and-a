d2d4b4b1fd76ca8a5394c54d69a80b6c9043033d
QuadDepo <ryan@qdds.nl>
2021-06-09 22:22:59 +0200

    Add sorting QA list to main

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import styled from "styled-components";
 import { Col, Container } from "./index";
 import { Title } from "../Typography";
 import Header from "./Header";
@@ -6,8 +8,37 @@ import QAForm from "../QAForm";
 import CarouselWrapper from "../CarouselWrapper";
 import Tooltip from "../Tooltip";
 
+const SortingSelect = styled.select`
+  padding: 8px 16px 8px 8px;
+`;
+
 const Main = () => {
+  const sortingOptions = [
+    {
+      key: "question",
+      direction: "ASC",
+      label: "Alphabetical Asc",
+    },
+    {
+      key: "question",
+      direction: "DESC",
+      label: "Alphabetical Desc",
+    },
+  ];
+
+  const [order, setOrder] = useState(sortingOptions[0]);
   const dispatch = useDispatch();
+
+  const onOrderChanged = ({ target }) => {
+    const { value } = target;
+    const [key, direction] = value.split("-");
+
+    const sortingOption = sortingOptions.find(
+      (item) => item.direction === direction && item.key === key
+    );
+
+    setOrder(sortingOption);
+  };
 
   return (
     <>
@@ -28,7 +59,12 @@ const Main = () => {
         </Col>
         <Col xs={12} sm={12} md={6} lg={6}>
           <Title>Q/A's</Title>
-          <CarouselWrapper />
+          <SortingSelect onChange={onOrderChanged}>
+            {sortingOptions.map(({ key, direction, label }) => (
+              <option value={`${key}-${direction}`}>{label}</option>
+            ))}
+          </SortingSelect>
+          <CarouselWrapper order={order} />
         </Col>
       </Container>
     </>
